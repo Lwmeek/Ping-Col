2e2d414058c8abe16733becc1b7fa14e95cf2a93
Lwmeek <timicivey27@gmail.com>
2022-11-02 07:22:54 -0400

    finishing javascript code

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ const email = document.querySelector("#email");
 const error = document.querySelector("#error");
 const button1 = document.querySelector(".btn-primary");
 
+const validEmail =
+		/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+function emailSuccess(){
+	if (email.value.match(validEmail)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	const validEmail =
-		/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	if (email.value.match(validEmail)) {
-		console.log("sumbitted");
-	} else {
-		error.textContent = "Please provide a valid email address";
-		button1.style.borderColor = "hsl(354, 100%, 66%)";
-		error.style.display = "block";
-	}
+	error.style.display = "none";
+	button1.style.borderColor = "hsl(223, 100%, 88%)";
+	error.style.color = "hsl(354, 100%, 66%)";
+
 	if (email.value.length === 0) {
 		error.textContent = "Whoops! It looks like you forgot to add your email";
 		error.style.display = "block";
 		button1.style.borderColor = "hsl(354, 100%, 66%)";
-		throw new Error("no email");
+	} else if (emailSuccess() === false) {
+		error.textContent = "Please provide a valid email address";
+		button1.style.borderColor = "hsl(354, 100%, 66%)";
+		error.style.display = "block";
+	} else {
+		console.log("sumbitted");
+		error.textContent = "You will be notified";
+		error.style.display = "block";
+		error.style.color = "hsl(223, 100%, 88%)";
 	}
-	return console.log(email.value);
 });
